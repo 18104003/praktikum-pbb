53a083b24325dc041463f3e4343dd7c2c0a48621
18104003 <72797572+18104003@users.noreply.github.com>
2022-02-05 11:58:53 +0700

    Add files via upload

---
+++ modul3/app/src/main/java/com/aji_18104003/modul3/Halaman2Activity.kt
@@ -0,0 +1,11 @@
+package com.aji_18104003.modul3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Halaman2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman2)
+    }
+}
